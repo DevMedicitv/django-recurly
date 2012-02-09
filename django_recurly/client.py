c518b9309847f5cb0680ae4a8059c4932db6987b
Adam Charnock <adam@omniwiki.co.uk>
2012-02-09 18:24:08 +0000

    Updating Manifest

--- django_recurly/client.py
+++ django_recurly/client.py
@@ -1,8 +1,8 @@
-from recurly import Recurly
-
 from django.conf import settings
 
 def get_client():
+    from recurly import Recurly
+    
     return Recurly(
         username=settings.RECURLY_USERNAME,
         password=settings.RECURLY_PASSWORD,
