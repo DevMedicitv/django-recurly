8e1b8e23273d953674f2827e7f346bfcaee742ba
Adam Charnock <adam@omniwiki.co.uk>
2012-02-11 22:16:39 +0000

    Updating package metadata

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setup(
     license='BSD',
     packages=find_packages(),
     version=__version__,
+    url="https://github.com/adamcharnock/django-recurly",
+    author="Adam Charnock",
+    author_email="adam@playnice.ly",
     
     install_requires=[
         "iso8601",
