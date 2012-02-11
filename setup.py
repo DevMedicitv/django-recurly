eab12087825d8e00da9488a5a6dfc3aff797fecf
Adam Charnock <adam@omniwiki.co.uk>
2012-02-11 21:55:05 +0000

    Minor adjustment to package layout

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 from setuptools import setup, find_packages
+from django_recurly import __version__
 
 setup(
     name='django-recurly',
     license='BSD',
     packages=find_packages(),
+    version=__version__,
     
     install_requires=[
         "iso8601",
