1f197ed83fd096a11becb0b43737428892da211a
Derrick Petzold <contact@derrickpetzold.com>
2012-08-13 14:47:20 -0700

    Added support for dumping plans.

--- django_recurly/management/commands/recurly.py
+++ django_recurly/management/commands/recurly.py
@@ -12,7 +12,8 @@ from optparse import make_option
 class JsonEncoder(json.JSONEncoder):
     def default(self, obj):
 
-        if isinstance(obj, datetime.datetime):
+        if isinstance(obj, datetime.datetime) or \
+                isinstance(obj, recurly.resource.Money):
             return str(obj)
 
         if callable(obj):
@@ -40,6 +41,7 @@ def dump(obj):
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
+
         make_option('--accounts',
             action='store_true',
             dest='accounts',
@@ -47,36 +49,54 @@ class Command(BaseCommand):
             help='List all the accounts'),
         make_option('--account',
             dest='account',
-            help='Get the specified account code'),
+            help='Get the specified account by code'),
+
         make_option('--subscription',
             dest='subscription',
-            help='Get the specified subscription uuid'),
+            help='Get the specified subscription by uuid'),
         make_option('--subscriptions',
             action='store_true',
             dest='subscriptions',
             default=False,
             help='List all the subscriptions'),
-        )
+
+        make_option('--plan',
+            dest='plan',
+            help='Get the specified plan by code'),
+        make_option('--plans',
+            action='store_true',
+            dest='plans',
+            default=False,
+            help='List all the subscriptions'),
+         )
 
     help = 'Display recurly data'
 
     def handle(self, *args, **options):
 
         if options['accounts']:
+            account = recurly.Account().get(options['account'])
+            print(dump(account))
+
+        elif options['account']:
             for account in recurly.Account().all_active():
                 print(dump(account))
 
-        elif options['subscriptions']:
-            for subscription in recurly.Subscription().all():
-                print(dump(subscription))
+        elif options['plan']:
+            plan = recurly.Plan().get(options['plan'])
+            print(dump(plan))
 
-        elif options['account']:
-            account = recurly.Account().get(options['account'])
-            print(dump(account))
+        elif options['plans']:
+            for plan in recurly.Plan().all():
+                print(dump(plan))
 
         elif options['subscription']:
             subscription = recurly.Subscription().get(options['subscription'])
             print(dump(subscription))
+
+        elif options['subscriptions']:
+            for subscription in recurly.Subscription().all():
+                print(dump(subscription))
 
         else:
             self.print_help(None, None)
